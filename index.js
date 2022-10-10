becdbe0f84f4960bf6aed2d239d46249073f1eef
Cahaya Edriansa <chyedriansa@gmail.com>
2022-10-10 09:47:23 -0700

    First Commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const readline = require('readline');
-// const rl = readline.createInterface(process.stdin, process.stdout);
 const cmd = readline.createInterface(process.stdin, process.stdout);
 
 var price = 8000;
-oprational = 0.045 * price;
-var reply = "n";
+const oprational = 0.045 * price;
+var reply = true;
 
-do {
-  cmd.question(' Berapa Jarak yang ditempuh? ', (ans) => {
+// while(reply){
+    cmd.question(' Berapa Jarak yang ditempuh? ', (ans) => {
     if (ans <= 2) {
-      // var price  = 8000;
-      console.log('Total Ongkos Kirim  :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(price))
 
-      // oprational = 0.045 * price;
+      console.log('Total Ongkos Kirim  :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(price))
       console.log('Biaya Layanan Sebesar : ' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(oprational))
 
       cmd.close()
@@ -30,8 +27,8 @@ do {
       cmd.close();
     }
 
-  });
-  cmd.question('apakah anda ingin  mengulang?', (reply))
-} while (reply == "y")
+});
+// }
+// cmd.question('apakah anda ingin  mengulang?', (reply))
 
-//code di line 35 ga ke eskusi
+//code di line 33 ga ke eskusi
