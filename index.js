9ac4457eb028145c68bf9048f7e46c66ef8b58b1
Cahaya Edriansa <chyedriansa@gmail.com>
2022-10-10 09:07:15 -0700

    Update FIX

--- index.js
+++ index.js
@@ -19,18 +19,19 @@ do {
     }
 
     else if (ans >= 3) {
+      price
       discount = 5000;
       final_price = price + (discount * (ans - 2))
       console.log('Total Ongkos Kirim  :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(final_price))
 
-      oprationals = 0.045 * price;
+      oprationals = 0.045 * final_price;
       console.log('Total Biaya Layanan : ' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(oprationals))
 
       cmd.close();
     }
 
   });
-  cmd.question('apakah anda ingin mengulang?', (reply))
+  cmd.question('apakah anda ingin  mengulang?', (reply))
 } while (reply == "y")
 
 //code di line 35 ga ke eskusi
