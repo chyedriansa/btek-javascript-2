596522f7dd59ca4612aa2b5a8a8fee53a03b15c5
Cahaya Edriansa <chyedriansa@gmail.com>
2022-10-12 04:53:59 -0700

    Bismillah

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const readline = require('readline');
+const {reject} = require('assert');
+const {resolve} = require('path');
 const cmd = readline.createInterface(process.stdin, process.stdout);
 
 var price = 8000;
 const oprational = 0.045 * price;
-var reply = true;
+let reply =  true;
+const error = () => {
+    cmd.close()
+  }
+const start = async() => {
+  let reply = true;
 
-// while(reply){
-    cmd.question(' Berapa Jarak yang ditempuh? ', (ans) => {
-    if (ans <= 2) {
+  while(reply){
+    try{
+        cmd.question(' Berapa Jarak yang ditempuh? ', (ans) => {
+        if (ans <= 2) {
 
-      console.log('Total Ongkos Kirim  :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(price))
-      console.log('Biaya Layanan Sebesar : ' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(oprational))
+          console.log('Biaya Ongkos Kirim :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(price))
+          console.log('Biaya Layanan Sebesar : ' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(oprational))
 
-      cmd.close()
+        //   cmd.close()
+        }
+
+        else if (ans >= 3) {
+          price
+          discount = 5000;
+          final_price = price + (discount * (ans - 2))
+          console.log('Total Ongkos Kirim  :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(final_price))
+
+          oprationals = 0.045 * final_price;
+          console.log('Total Biaya Layanan : ' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(oprationals))
+
+        //   cmd.close();
+        }
+
+    })
+
     }
-
-    else if (ans >= 3) {
-      price
-      discount = 5000;
-      final_price = price + (discount * (ans - 2))
-      console.log('Total Ongkos Kirim  :' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(final_price))
-
-      oprationals = 0.045 * final_price;
-      console.log('Total Biaya Layanan : ' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(oprationals))
-
-      cmd.close();
+    catch(error){
+        console.log("DONE!!!");
     }
+  }
+}
+start();
+cmd.question('apakah anda ingin  mengulang?', (reply))
 
-});
-// }
-// cmd.question('apakah anda ingin  mengulang?', (reply))
-
-//code di line 33 ga ke eskusi
+//code di line 32 ga ke eskusi
